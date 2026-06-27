7932089653384be062f8691c4ca5921388e154d7
Grogu <patrick.ottley.vssa@gmail.com>
2026-06-26 23:07:50 -0400

    Change base path in Vite configuration
    To attach a domain from wix the path was PatrickOttleyPortfolio but the DNS can only add on / so changing it to allow.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PatrickOttleyPortfolio/',
+  base: '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
